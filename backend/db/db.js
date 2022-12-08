c3502eaefdec22b6e302fe7c6323145ec790e70e
geronimo <geronicola1696@gmail.com>
2022-12-08 16:26:16 -0300

    + variables de entorno

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,8 +3,9 @@ const { Sequelize, Model, DataTypes } = require("sequelize");
 
 console.log(process.env.DB_NAME)
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect:  'postgres', 
+  host: process.env.DB_HOST,
+  dialect:  process.env.DB_DRIVER, 
+  port: process.env.DB_PORT,
 });
 
  
